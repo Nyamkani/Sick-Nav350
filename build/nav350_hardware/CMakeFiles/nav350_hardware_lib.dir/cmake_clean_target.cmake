6d8ef8cf94c0a5eaceb070c9b98f67008948d9af
Nyamkani <yapyapchicken@gmail.com>
2022-07-05 09:06:19 +0900

    070522

--- build/nav350_hardware/CMakeFiles/nav350_hardware_lib.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libnav350_hardware_lib.a"
-)
--- build/nav350_hardware/CMakeFiles/nav350_hardware_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/nav350_hardware_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/nav350_hardware_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
